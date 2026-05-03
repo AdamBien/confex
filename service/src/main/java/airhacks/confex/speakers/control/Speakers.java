0abc217c0ec6e2bb01745b5ffef3c59df8e8e311
Adam Bien <abien@adam-bien.com>
2026-05-03 18:00:46 +0200

    refactoring: control introduced / conflict detection implemented

---
+++ service/src/main/java/airhacks/confex/speakers/control/Speakers.java
@@ -0,0 +1,32 @@
+package airhacks.confex.speakers.control;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import airhacks.confex.speakers.entity.Speaker;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ClientErrorException;
+import jakarta.ws.rs.core.Response;
+
+@ApplicationScoped
+public class Speakers {
+
+    List<Speaker> speakers = new CopyOnWriteArrayList<>();
+
+    public List<Speaker> all() {
+        return List.copyOf(this.speakers);
+    }
+
+    public void add(Speaker speaker) {
+        if (exists(speaker.identifier())) {
+            throw new ClientErrorException(Response.Status.CONFLICT);
+        }
+        this.speakers.add(speaker);
+    }
+
+    boolean exists(String identifier) {
+        return this.speakers.stream()
+                .map(Speaker::identifier)
+                .anyMatch(identifier::equals);
+    }
+}
